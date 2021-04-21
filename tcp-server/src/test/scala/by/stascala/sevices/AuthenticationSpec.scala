059d080a48bb39777e5c35ed445b1f2c7a03d711
Stanislau Buhlak <staniopolis@gmail.com>
2021-04-21 15:24:40 +0300

    gitignore added

--- tcp-server/src/test/scala/by/stascala/sevices/AuthenticationSpec.scala
+++ tcp-server/src/test/scala/by/stascala/sevices/AuthenticationSpec.scala
@@ -2,7 +2,11 @@ package by.stascala.sevices
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActors, TestKit}
-import by.stascala.api.{CurrentBalance, SingUp}
+import by.stascala.GameSession
+import by.stascala.api.CurrentBalance
+import by.stascala.services.Authentication.{LogIn, SingUp}
+import by.stascala.services.LobbyRoom.TokensHold
+import by.stascala.services.Persistent.Hold
 import by.stascala.services.{Authentication, Persistent}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
@@ -24,11 +28,17 @@ class AuthenticationSpec extends TestKit(ActorSystem("MySpec"))
 
   "Authentication actor" must {
 
-    "send initial balance 1000 tokens" in {
-      testAuth ! SingUp("TestPlayer", "123")
+    "receive initial balance" in {
+      testAuth ! SingUp("TestPlayer", "", testActor)
       expectMsg(CurrentBalance(1000L))
     }
-
+    "receive player's current balance" in {
+      testPers ! Hold("TestPlayer", 100L)
+      expectMsg(TokensHold)
+      testAuth ! LogIn("TestPlayer", "", testActor)
+      expectMsg(CurrentBalance(900L))
+    }
   }
+
 
 }
